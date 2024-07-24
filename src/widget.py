2bde5568a70a91ca3b7894136febc7cf63153661
Erik Husainov <erik.husainov.1997@mail.ru>
2024-07-24 22:36:29 +0500

    message

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,8 @@ import masks
 
 
 def mask_account_card(acc_card: str) -> str:
+    """Функция возвращает маску счета или карты в соответвующем формате"""
+
     if len(acc_card.split()[-1]) == 20:
         return f'{' '.join(acc_card.split()[:-1])} {masks.get_mask_account(int(acc_card.split()[-1]))}'
 
@@ -9,4 +11,6 @@ def mask_account_card(acc_card: str) -> str:
 
 
 def get_date(date: str) -> str:
+    """Функция возвращает дату в формате ДД.ММ.ГГГГ"""
+
     return '.'.join(date[:10].split('-')[::-1])
