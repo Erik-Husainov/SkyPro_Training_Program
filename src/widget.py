19d258105e1f0d305bf2b4b52122904197426b7c
Erik Husainov <erik.husainov.1997@mail.ru>
2024-07-24 22:31:38 +0500

    message

---
+++ src/widget.py
@@ -0,0 +1,8 @@
+import masks
+
+
+def mask_account_card(acc_card: str) -> str:
+    if len(acc_card.split()[-1]) == 20:
+        return f'{' '.join(acc_card.split()[:-1])} {masks.get_mask_account(int(acc_card.split()[-1]))}'
+
+    return f'{' '.join(acc_card.split()[:-1])} {masks.get_mask_card_number(int(acc_card.split()[-1]))}'
