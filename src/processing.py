86831c6710fa161d466e1aed50ebdba953d9b379
Erik Husainov <erik.husainov.1997@mail.ru>
2024-07-29 22:14:33 +0500

    init commit

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+from widget import get_date
+
+
+def filter_by_state(dicts_list: list, state: str = 'EXECUTED'):
+    """Функция принимает список словарей и опциональный параметр state и возвращает новый список словарей,
+    у которых соответствующий state"""
+
+    return [i for i in dicts_list if i['state'] == state]
+
+
+def sort_by_date(dicts_list: list, sort_way: str = 'descending'):
+    return [sorted(dicts_list, key=lambda x: get_date(x['date']), reverse=(sort_way == 'descending'))]
+
+
+
+d = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+print(sort_by_date(d))
