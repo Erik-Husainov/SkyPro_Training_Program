214c665667263f4293bec9c40cef9b682715b7c6
Erik Husainov <erik.husainov.1997@mail.ru>
2024-08-07 20:10:15 +0500

    Изменены функции, исправлены ошибки

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
-from src import widget
-
+from src.widget import *
+from datetime import datetime
 
 def filter_by_state(dicts_list: list, state: str = 'EXECUTED'):
     """Функция принимает список словарей и опциональный параметр state и возвращает новый список словарей,
@@ -9,7 +9,9 @@ def filter_by_state(dicts_list: list, state: str = 'EXECUTED'):
 
 
 def sort_by_date(dicts_list: list, sort_way: bool = True):
-    return [sorted(dicts_list, key=lambda x: get_date(x['date']), reverse=(not(sort_way)))]
+    return [sorted(dicts_list, key=lambda x: datetime.strptime(get_date(x['date']),"%d.%m.%Y"), reverse=(not sort_way))]
 
-
-
+print(sort_by_date([{'id': 4, 'state': 'EXECUTED', 'date': '2019-07-03T'},
+            {'id': 9, 'state': 'EXECUTED', 'date': '2018-06-30T'},
+            {'id': 5, 'state': 'CANCELED', 'date': '2018-09-12T'},
+            {'id': 6, 'state': 'CANCELED', 'date': '2018-10-14T'}], 'CANCELED'))
