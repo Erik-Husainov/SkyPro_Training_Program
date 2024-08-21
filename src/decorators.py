41688926b0913ec10690904d7e43d274c9331e0c
Erik Husainov <erik.husainov.1997@mail.ru>
2024-08-21 22:28:35 +0500

    Удалил функцию для проверки и сделал перенос длинной строки

--- src/decorators.py
+++ src/decorators.py
@@ -3,13 +3,14 @@ def log(filename=None):
     Если имя файла не указано, то выводит в консоль"""
     def process(func):
         """Дополнительный декоратор, принимающий на вход функцию"""
-        def wrapper(*args, **kwargs) -> str:
+        def wrapper(*args, **kwargs) -> None:
             """Функция-обертка, в которой обрабатываются все выходные данные функции"""
             try:
                 result = func(*args, **kwargs)
                 text = f'Function {func.__name__} is working. Result: {result}'
             except Exception as error:
-                text = f'An error occurred while the function was running: {str(error)}. Input data: {args}, {kwargs}; filename: {filename}'
+                text = (f'An error occurred while the function was running: {str(error)}. Input data: {args}, {kwargs};'
+                        f' filename: {filename}')
             if filename:
                 f = open(filename, 'a')
                 f.write(text+'\n')
@@ -18,9 +19,3 @@ def log(filename=None):
                 print(text)
         return wrapper
     return process
-
-@log(filename='ret.txt')
-def funcv(x, y):
-    return x + y
-
-funcv(1)
